44d2b4a00179ae0f45de712083e2cd4d017f26e8
Marco Novaes <marco-novaes@uol.com.br>
2021-11-17 00:58:57 -0300

    Build the OrderApi, Change login layout page from ConstraintLayou to Linear Layout. Firebase configuration. Firebase Login Authentication ready.

--- app/src/main/java/com/ccm/purchaseorder_ccm/MainActivity.java
+++ app/src/main/java/com/ccm/purchaseorder_ccm/MainActivity.java
@@ -1,18 +1,123 @@
 //TODO FOCUS ON DESIGN FIRST!
 package com.ccm.purchaseorder_ccm;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Objects;
 
+import util.OrderApi;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+
+    private AutoCompleteTextView emailAddress;
+    private EditText password;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(super.getSupportActionBar()).hide(); //Hides the ActionBar
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        emailAddress =  findViewById(R.id.email);
+        password =  findViewById(R.id.password);
+
+        loginButton = findViewById(R.id.button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginEmailPasswordUser(emailAddress.getText().toString().trim(),
+                        (password.getText()).toString().trim());
+            }
+        });
+
+    }
+
+    private void loginEmailPasswordUser(String email, String pwd){
+        if(!TextUtils.isEmpty(email)
+        && !TextUtils.isEmpty(pwd)){
+            firebaseAuth.signInWithEmailAndPassword(email, pwd)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            assert user != null;
+                            String currentUserId = user.getUid();
+
+                            collectionReference
+                                    .whereEqualTo("userId", currentUserId)
+                                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                                            if(error !=null){
+
+                                            }
+                                            assert value != null;
+                                            if(!value.isEmpty()){
+                                                for(QueryDocumentSnapshot snapshot : value){
+                                                    OrderApi orderApi = OrderApi.getInstance();
+                                                    orderApi.setUsername(snapshot.getString("username"));
+                                                    orderApi.setUserId(snapshot.getString("userId"));
+
+                                                    //Go to Purchase Order List
+                                                    startActivity(new Intent(MainActivity.this,
+                                                            PurchaseOrderList.class));
+
+
+                                                }
+                                            }
+                                        }
+                                    });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+
+                    }else {
+            Toast.makeText(MainActivity.this,
+                    "Please enter email and password",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 }
