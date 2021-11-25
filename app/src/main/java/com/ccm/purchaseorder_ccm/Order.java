7f582422e5cfeb2599dc0cf6821a8a329c645240
Marco Novaes <marco-novaes@uol.com.br>
2021-11-24 21:13:09 -0300

    Create Client.java and Order.java.

---
+++ app/src/main/java/com/ccm/purchaseorder_ccm/Order.java
@@ -0,0 +1,32 @@
+package com.ccm.purchaseorder_ccm;
+
+public class Order {
+    private String ClientId;
+    private String OrderId;
+
+    public Order(String clientId, String orderId) {
+        ClientId = clientId;
+        OrderId = orderId;
+    }
+
+    public String getClientId() {
+        return ClientId;
+    }
+
+    public void setClientId(String clientId) {
+        ClientId = clientId;
+    }
+
+    public String getOrderId() {
+        return OrderId;
+    }
+
+    public void setOrderId(String orderId) {
+        OrderId = orderId;
+    }
+
+    public Order() {
+    }
+
+
+}
