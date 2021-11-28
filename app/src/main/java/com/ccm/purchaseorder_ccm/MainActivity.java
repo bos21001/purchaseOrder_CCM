c2ba666a5b2e3c9fb26fa362017a475675788070
Christopher Mendes <bos21001@byui.edu>
2021-11-27 21:48:29 -0300

    Creating a new branch to test

--- app/src/main/java/com/ccm/purchaseorder_ccm/MainActivity.java
+++ app/src/main/java/com/ccm/purchaseorder_ccm/MainActivity.java
@@ -1,9 +1,5 @@
-//TODO FOCUS ON DESIGN FIRST!
+//TODO TEST ONLY!!
 package com.ccm.purchaseorder_ccm;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,23 +10,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Objects;
-
-import util.OrderApi;
 
 public class MainActivity extends AppCompatActivity {
 
